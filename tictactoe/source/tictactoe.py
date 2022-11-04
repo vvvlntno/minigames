1880c034afa787f624acfc75e45f1370a462b1e7
valentino <82881487+vvvlntno@users.noreply.github.com>
2022-11-04 15:03:30 +0100

    first functionality of the bot

--- tictactoe/source/tictactoe.py
+++ tictactoe/source/tictactoe.py
@@ -2,6 +2,7 @@
 # Date: 03.11.2022
 
 import re
+import random
 
 class Board:
     def __init__(self, values: list=["-","-","-","-","-","-","-","-","-"],winner: str="") -> None:
@@ -53,8 +54,41 @@ class Board:
             return [False, "Please give a validate input (a-c1-3)"]
         if not self.__checkinputforfieldusage(userinp):
             return [False, "Please input a field that is not used"]
-        setattr(self,userinp,turn) #set value
+        self.setvalue(userinp, turn) #set value
         return [True, ""]
+
+
+    def __checkifvarisavailable(self, var):
+        if (var=="X" or var=="O"):
+            return False
+        return True
+
+    def createlistofvalidelements(self):
+        validoptions = []
+        if(self.__checkifvarisavailable(self.a1)):
+            validoptions.append("a1")
+        if(self.__checkifvarisavailable(self.a2)):
+            validoptions.append("a2")
+        if(self.__checkifvarisavailable(self.a3)):
+            validoptions.append("a3")
+        if(self.__checkifvarisavailable(self.b1)):
+            validoptions.append("b1")
+        if(self.__checkifvarisavailable(self.b2)):
+            validoptions.append("b2")
+        if(self.__checkifvarisavailable(self.b3)):
+            validoptions.append("b3")
+        if(self.__checkifvarisavailable(self.c1)):
+            validoptions.append("c1")
+        if(self.__checkifvarisavailable(self.c2)):
+            validoptions.append("c2")
+        if(self.__checkifvarisavailable(self.c3)):
+            validoptions.append("c3")
+        return validoptions
+    
+
+    def setvalue(self, inp: str, turn: str):
+        setattr(self,inp,turn)
+
 
     def printgameboard(self):
         print("    a   b   c")
@@ -83,6 +117,22 @@ class Board:
         self.values = [self.a1,self.a2,self.a3,self.b1,self.b2,self.b3,self.c1,self.c2,self.c3]
         self.winner = ""
 
+
+class Bot:
+    def __init__(self, indicator) -> None:
+        self.indicator = indicator
+    
+    def getrandomchoice(self,validoptions: list) -> list:
+        return random.choice(validoptions)
+
+
+#bot: list of valid options (a1,a2,a3,...)
+#create valid list
+# for val in values if value does not have "x" or "o" then add to list
+
+#bot class, init with indicator "x" or "o"
+#give list of elements and return option
+
 def inputsequence(turn: str, bf: Board):
     bf.printgameboard()
     userinput = input(f"\n{turn}: Where do you want to place it?")
@@ -92,7 +142,14 @@ def inputsequence(turn: str, bf: Board):
         userinput = input(f"{turn}: Where do you want to place it?")
         validated = bf.checkinputandsetvalue(userinput, turn)
 
-def gamesquence():
+
+def botsequence(turn: str, bf: Board, bt: Bot):
+    botchoice = bt.getrandomchoice(bf.createlistofvalidelements())
+    bf.setvalue(botchoice, turn)
+    bf.printgameboard()
+
+
+def gamesequence():
     B = Board()
     loop = B.checkforend()
     while not loop:
@@ -110,8 +167,51 @@ def gamesquence():
     B.printgameboard()
     B.printwinner()
 
+
+def botgamesequence():
+    B = Board()
+    loop = B.checkforend()
+    BT = Bot("O")
+    while not loop:
+        B.renewvaluelist()
+        inputsequence("X", B)
+        if (B.checkforwin("X")):
+            loop = True
+            break
+        
+        botsequence(BT.indicator, B, BT)
+        loop = B.checkforend()
+        if (B.checkforwin("O")):
+            loop = True
+            break
+    B.printgameboard()
+    B.printwinner()
+
 def main():
-    gamesquence()
+    inp = input("Please type if you want to play with a bot (b) or with a friend (n): ")
+    if(inp=="n"):
+        gamesequence()
+    elif(inp=="b"):
+        botgamesequence()
+    else:
+        print("Please restart and Input 'n' or 'b'")
 
 if __name__=="__main__":
     main()
+
+
+
+#test
+# B = Board()
+# B.printgameboard()
+# B.setvalue("a1", "X")
+
+# rrlist = B.createlistofvalidelements()
+# print(rrlist)
+# Bot1 = Bot("O")
+# botchoice = Bot1.getrandomchoice(rrlist)
+
+# B.setvalue(botchoice, Bot1.indicator)
+
+# B.printgameboard()
+
